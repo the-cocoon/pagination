79f204c6e7c1f33952fb917c89d6644a0acfc523
Ilya N. Zykin <zykin-ilya@ya.ru>
2015-11-16 13:33:27 +0300

    first_commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pagination
+
+Just Pagination conerns
---
+++ lib/pagination.rb
@@ -0,0 +1,15 @@
+require "pagination/version"
+
+module Pagination
+  class Engine < Rails::Engine; end
+
+  module Base
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+      def pagination params
+        page(params[:page]).per(params[:per_page])
+      end
+    end
+  end
+end
